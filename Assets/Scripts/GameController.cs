a120e06d6cd3d6f31568ee9610d33f3e5f888921
Sean Campbell <campbellsean211@gmail.com>
2019-09-18 16:17:41 -0400

    Board & Gamecontroller
    Got a board up with placeholder tiles and the GameController script fills a 2d array with the tiles in their visual order

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //Public Variables
+    public GameObject board;
+
+    //Private Variables
+    private int boardWidth = 7;
+    private int boardHeight = 12;
+    private GameObject[,] boardArray = new GameObject[12, 7];
+
+
+    // Instantiate & Preprocess
+    void Start()
+    {
+        GameObject currentRow;
+        //Creates an array filled with tile game objects
+        for(int i = 0; i < boardHeight; i++)
+        {
+            currentRow = board.transform.GetChild(i).gameObject;
+            for(int j = 0; j < boardWidth; j++)
+            {
+                boardArray[i, j] = currentRow.transform.GetChild(j).gameObject;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
