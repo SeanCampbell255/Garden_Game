112268ca6479da0d30b81840dbd0d9266acdff5c
Sean Campbell <campbellsean211@gmail.com>
2019-09-18 17:19:39 -0400

    Player Schmoves
    Player moves by changing which tile is it's parent

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -28,9 +28,7 @@ public class GameController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public GameObject[,] GetBoardArray(){
+        return boardArray;
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+    //Public Variables
+    public GameController gameController;
+
+    //Private Variables
+    private GameObject[,] boardArray;
+
+    private int playerPosition = 3;
+
+    //Instantiation
+    private void Start(){
+        boardArray = gameController.GetBoardArray();
+    }
+
+    void Update(){
+        bool moveLeft = Input.GetKeyDown(KeyCode.LeftArrow);
+        bool moveRight = Input.GetKeyDown(KeyCode.RightArrow);
+
+        if (moveLeft && playerPosition > 0){
+            playerPosition--;
+        }else if (moveRight && playerPosition < 6){
+            playerPosition++;
+        }
+        UpdatePlayerPosition();
+    }
+
+    void UpdatePlayerPosition(){
+        GameObject targetTile = boardArray[11, playerPosition];
+
+        this.transform.SetParent(targetTile.transform, false);
+    }
+}
