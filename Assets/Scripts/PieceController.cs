5ecf6ff13a50a878c2e5c2936b56893bfbdfd6cb
Sean Campbell <campbellsean211@gmail.com>
2019-09-18 21:30:44 -0400

    Gonna Scrap Piece Adjacency
    There's going to be issues with updating adjacencies if it's just piece adjacency. I'm gonna revert all this and find a better solution

--- Assets/Scripts/PieceController.cs
+++ Assets/Scripts/PieceController.cs
@@ -19,7 +19,11 @@ public class PieceController : MonoBehaviour
 
     // Start is called before the first frame update
     void Awake(){
+        gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+
         adjacencyArray = gameController.GetAdjacencies(xPosition, yPosition);
+
+        SetAdjacencies();
     }
 
     // Update is called once per frame
@@ -31,5 +35,20 @@ public class PieceController : MonoBehaviour
     void SetPosition(int x, int y){
         xPosition = x;
         yPosition = y;
+    }
+
+    void SetAdjacencies(){
+        if(adjacencyArray[0] != null){
+            abovePiece = adjacencyArray[0];
+        }
+        if(adjacencyArray[1] != null){
+            rightPiece = adjacencyArray[1];
+        }
+        if(adjacencyArray[2] != null){
+            belowPiece = adjacencyArray[2];
+        }
+        if(adjacencyArray[3] != null){
+            leftPiece = adjacencyArray[3];
+        }
     }
 }
