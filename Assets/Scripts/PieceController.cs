9a42c07e89a52187d5e0c01893cf869cf4b28a82
Sean Campbell <campbellsean211@gmail.com>
2019-09-20 15:18:16 -0400

    Temp Sprites Done
    Sprites are set when the type is set for each piece. Trash sprites are always randomized upon placement, can swap for saving their sprites in order later

--- Assets/Scripts/PieceController.cs
+++ Assets/Scripts/PieceController.cs
@@ -8,8 +8,17 @@ public class PieceController : MonoBehaviour
 
     public GameController.PieceType type;
 
+    public Sprite[] trashSprites;
+    public Sprite seedSprite;
+    public Sprite sproutSprite;
+    public Sprite budSprite;
+    public Sprite flowerSprite;
+
+    private SpriteRenderer spriteRenderer;
+
     void Awake(){
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,6 +29,24 @@ public class PieceController : MonoBehaviour
    
     public void SetType(GameController.PieceType type){
         this.type = type;
+
+        if(type == GameController.PieceType.Trash){
+            int spriteNum = (int)Random.Range(0.0f, 3.9f);
+            spriteRenderer.sprite = trashSprites[spriteNum];
+
+        }else if (type == GameController.PieceType.Seed){
+            spriteRenderer.sprite = seedSprite;
+
+        }else if (type == GameController.PieceType.Sprout){
+            spriteRenderer.sprite = sproutSprite;
+
+        }else if (type == GameController.PieceType.Bud){
+            spriteRenderer.sprite = budSprite;
+
+        }else if (type == GameController.PieceType.Flower){
+            spriteRenderer.sprite = flowerSprite;
+
+        }
     }
 
     public GameController.PieceType GetType(){
