ae189e541307a1a90fe1b3414ca5523ff28ecf73
Sean Campbell <campbellsean211@gmail.com>
2019-10-02 19:10:45 -0400

    Working Matching
    Matching removes pieces from board, all that's left is spawning a piece and checking matching on that

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,6 +11,8 @@ public class GameController : MonoBehaviour
 
     public enum PieceType {Trash, Seed, Sprout, Bud, Flower };
 
+    public int matchSize;
+
     //Private Variables
     private int boardWidth = 7;
     private int boardHeight = 12;
@@ -20,6 +22,7 @@ public class GameController : MonoBehaviour
     
 
     private GameObject[,] boardArray = new GameObject[7, 12];
+    private List<int[]> matchingCoordinates = new List<int[]>();
 
 
     // Instantiate & Preprocess
@@ -97,13 +100,96 @@ public class GameController : MonoBehaviour
                         i++;
                         basketSize--;
                     }
+                    CheckForMatch(tilePosition, basketType);
+                    ExecuteMatch();
                     break;
                 }
             }
         }
     }
 
+    private void CheckForMatch(int[] tilePosition, PieceType matchType){
+        Debug.Log("Checking at pos: " + tilePosition[0] + " " + tilePosition[1]);
+        matchingCoordinates.Add(tilePosition);
+
+        int[] leftCoords = {tilePosition[0] - 1, tilePosition[1]};
+        int[] rightCoords = {tilePosition[0] + 1, tilePosition[1]};
+        int[] topCoords = {tilePosition[0], tilePosition[1] - 1};
+        int[] botCoords = {tilePosition[0], tilePosition[1] + 1};
+
+        GameObject leftPiece = GetPiece(leftCoords);
+        GameObject rightPiece = GetPiece(rightCoords);
+        GameObject topPiece = GetPiece(topCoords);
+        GameObject botPiece = GetPiece(botCoords);
+
+        if(leftPiece != null && !DoCoordinatesMatch(leftCoords)){
+            if(leftPiece.GetComponent<PieceController>().type == matchType){
+                CheckForMatch(leftCoords, matchType);
+            }
+        }
+        if(rightPiece != null && !DoCoordinatesMatch(rightCoords)){
+            if(rightPiece.GetComponent<PieceController>().type == matchType){
+                CheckForMatch(rightCoords, matchType);
+            }
+        }
+        if(topPiece != null && !DoCoordinatesMatch(topCoords)){
+            if(topPiece.GetComponent<PieceController>().type == matchType){
+                CheckForMatch(topCoords, matchType);
+            }
+        }
+        if(botPiece != null && !DoCoordinatesMatch(botCoords)){
+            if(botPiece.GetComponent<PieceController>().type == matchType){
+                CheckForMatch(botCoords, matchType);
+            }
+        }
+    }
+
+    private void ExecuteMatch(){
+        if(matchingCoordinates.Count >= matchSize){
+            foreach(int[] coord in matchingCoordinates){
+                Debug.Log("Destroying at " + coord[0] + " " + coord[1]);
+                DestroyImmediate(GetPiece(coord));
+            }
+            foreach(int[] coord in matchingCoordinates){
+                int[] botCoord = { coord[0], coord[1] + 1};
+                GameObject botPiece = GetPiece(botCoord);
+
+                while(botPiece != null){
+                    Debug.Log(botPiece.GetComponent<PieceController>().type + " at " + botCoord[0] + " " + botCoord[1]);
+                    botPiece.transform.SetParent(FindHighestEmptyTile(botCoord[0]).transform, false);
+
+                    botCoord[1]++;
+                    botPiece = GetPiece(botCoord);
+                }
+            }
+        }
+        matchingCoordinates.Clear();
+    }
+
+    private bool DoCoordinatesMatch(int[] coords){
+        foreach(int[] oldCoords in matchingCoordinates){
+            if(oldCoords[0] == coords[0] && oldCoords[1] == coords[1]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private GameObject FindHighestEmptyTile(int column){
+        for(int i = 0; i < 12; i++){
+            if(boardArray[column, i].transform.childCount == 0){
+                Debug.Log("Highest empty tile at: " + column + " " + i);
+                return boardArray[column, i];
+            }
+        }
+        return null;
+    }
+
     private GameObject GetPiece(int[] tilePos){
+        if(tilePos[0] < 0 || tilePos[0] > 6 || tilePos[1] < 0 || tilePos[1] > 11){
+            return null;
+        }
+
         Transform tileTransform = boardArray[tilePos[0], tilePos[1]].transform;
         if(tileTransform.childCount > 1){
             return tileTransform.GetChild(1).gameObject;
