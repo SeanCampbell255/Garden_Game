8981c931bf9969fdfe443469393e588bead1405a
Sean Campbell <campbellsean211@gmail.com>
2019-11-11 18:02:35 -0500

    Set up input handling Player.cs
    Also the matrix in Game.cs

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    //Public Instance Vars
+
+    //Private Instance Vars
+    private GameObject[,] boardMatrix = new GameObject[7, 12];
+
+    private void Update()
+    {
+        
+    }
+
+    public void AddToMatrix(GameObject entity, int[] coords)
+    {
+        boardMatrix[coords[0], coords[1]] = entity;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Public Instance Vars
+    public Game game;
+
+    public int moveTime;
+
+    //Private Instance Vars
+    private int[] coords = { 3, 11 };
+
+    private bool inputBlocked = false;
+
+    private float transformConstant = 4.5f;
+
+    private void Update()
+    {
+        if (!inputBlocked)
+        {
+            float horInput = Input.GetAxis("Horizontal");
+            float vertInput = Input.GetAxis("Vertical");
+
+            if (horInput != 0.0f)
+            {
+                if(horInput > 0.0f && coords[0] < 6)
+                {
+                    Move(1);
+                } else if (coords[0] > 0)
+                {
+                    Move(-1);
+                }
+            } else if (Input.GetAxis("Vertical") != 0.0f)
+            {
+
+
+            }
+        }
+    }
+
+    //Takes direction on x-axis to move player
+    private void Move(int direction)
+    {
+        inputBlocked = true;
+
+        coords[0] += direction;
+        game.AddToMatrix(this.gameObject, coords);
+    }
+
+    private IEnumerator AnimateMove()
+    {
+        //IMPLEMENT: Play animation & update position
+        yield return new WaitForSeconds(moveTime);
+    }
+}
