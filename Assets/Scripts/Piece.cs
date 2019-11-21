945592fb0039a421399fc0ecd13b87cf87c94c80
Sean Campbell <campbellsean211@gmail.com>
2019-11-21 12:53:30 -0500

    Piece Movement Implemented

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -9,6 +9,8 @@ public class Piece : MonoBehaviour
 
     public Game game;
 
+    public float moveTime = 0.33f;
+
     //Private Instance Vars
     private Type type;
 
@@ -28,10 +30,27 @@ public class Piece : MonoBehaviour
 
     public void Move(int[] targetCoords)
     {
+        int vector = coords[1] - targetCoords[1];
+
         game.RemoveFromMatrix(coords);
 
         game.AddToMatrix(this.gameObject, targetCoords);
 
         //Play anim & change position
+    }
+
+    private IEnumerator MoveAnim(int vector)
+    {
+        float originX = gameObject.transform.position.x;
+        float originY = gameObject.transform.position.y;
+        float move = (0.9f * vector) / 8;
+        float interval = moveTime / 8;
+
+        for (int i = 1; i <= 8; i++)
+        {
+            gameObject.transform.position = new Vector2(originX, originY + (move * i));
+
+            yield return new WaitForSeconds(interval);
+        }
     }
 }
